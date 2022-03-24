7f492c12e336ab17bab0f1b31c0933542dcc284e
Bryan Sullivan <code@brysull.com>
2022-03-24 09:20:11 -0500

    m2-b

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+// var ctx = document.getElementById("canvas").getContext("2d");
+// var img = new Image();
+// img.src = 'assets/sky.png'
+// img.onload = function() {
+//   var pat = ctx.createPattern(img, 'repeat');
+//   ctx.rect(0, 0, window.innerWidth, window.innerHeight);
+//   ctx.fillStyle = pat;
+//   ctx.fill();
+// };
+
+
+function newImage (imgPath, imgLeft, imgBottom) {
+    let imgPlace = document.createElement('img')
+    imgPlace.src = imgPath
+    imgPlace.style.position = 'fixed'
+    imgPlace.style.left = imgLeft + 'px'
+    imgPlace.style.bottom = imgBottom + 'px'
+    document.body.append(imgPlace)
+    return imgPlace
+}
+
+function newItem (imgPath, imgLeft, imgBottom) {
+    let pickUp = newImage (imgPath, imgLeft, imgBottom)
+
+    pickUp.addEventListener ('dblclick', () => {
+        pickUp.remove()
+    } )
+}
+
+newImage ('/assets/green-character.gif', 100, 100)
+newImage ('/assets/pine-tree.png', 450, 200)
+newImage ('assets/tree.png', 200, 300)
+newImage ('assets/pillar.png', 350, 100)
+newImage ('assets/crate.png', 150, 200)
+newImage ('assets/well.png', 500, 425)
+
+newItem ('assets/sword.png', 500, 405)
+newItem ('assets/sheild.png', 165, 185)
+newItem ('assets/staff.png', 600, 100)
+
+// bry5ull
